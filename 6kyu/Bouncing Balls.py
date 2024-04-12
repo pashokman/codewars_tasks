b9a703297db3cd7cb1edce84bb13d6474b6c3cea
Pavlo Lekhitskyi <pashokman1@gmail.com>
2024-04-12 18:09:28 +0300

    plus one more task

---
+++ 6kyu/Bouncing Balls.py
@@ -0,0 +1,45 @@
+"""
+A child is playing with a ball on the nth floor of a tall building. 
+The height of this floor above ground level, h, is known.
+
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+His mother looks out of a window 1.5 meters from the ground.
+
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing)?
+
+Three conditions must be met for a valid experiment:
+Float parameter "h" in meters must be greater than 0
+Float parameter "bounce" must be greater than 0 and less than 1
+Float parameter "window" must be less than h.
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+Note:
+The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+Examples:
+- h = 3, bounce = 0.66, window = 1.5, result is 3
+
+- h = 3, bounce = 1, window = 1.5, result is -1 
+
+(Condition 2) not fulfilled).
+"""
+
+def bouncing_ball(h, bounce, window):
+    if h <= 0 or bounce <= 0 or bounce >= 1 or window >= h:
+        return -1
+    else:
+        count = 1
+        bounce_height = bounce * h
+        while bounce_height > window:
+            bounce_height *= bounce
+            count += 2
+
+        return count
+
+
+
+print(bouncing_ball(2, 0.5, 1), 1)
+print(bouncing_ball(3, 0.66, 1.5), 3)
+print(bouncing_ball(30, 0.66, 1.5), 15)
+print(bouncing_ball(30, 0.75, 1.5), 21)
